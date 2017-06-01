ef94e824c3f3d1fef09a7c61c664d396aad52e60
Jeroen Bosch <jeroon.bosch@gmail.com>
2017-06-01 13:57:55 +0300

    Hot n spicy

--- Assets/Scripts/Constants/Constants.cs
+++ Assets/Scripts/Constants/Constants.cs
@@ -6,8 +6,8 @@ public static class Constants {
 
 	public const string Controller = "Mouse";
 	
-	public const int PlayerStartHP = 100;
-    public const float MoveTimeInSeconds = 10f;
+	public const int PlayerStartHP = 200;
+    public const float MoveTimeInSeconds = 20f;
 
     public const float SpecialMoveFillRequirement = 30f; //How much the maximum of the chosen 'special' is
     public const float SpecialMoveMultiplier = 2f; //How much the 'special' bar of a color fills up.
